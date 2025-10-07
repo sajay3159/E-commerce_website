7c75ec5b1ac42c88dd1b17990dd280540e65eb43
Ajay Sahani <sajay3159@gmail.com>
2025-10-07 11:42:10 +0530

    Adding Lazy loading and optimizing code for deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useContext, useState } from "react";
+import { lazy, Suspense, useContext, useState } from "react";
 import "./App.css";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Header/Header";
-import Products from "./components/Products/Products";
 import CartProvider from "./components/store/CartProvider";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import About from "./components/About/About";
-import Home from "./components/Home/Home";
-import ContactUs from "./components/ContactUs/ContactUs";
-import ProductDetail from "./components/Products/ProductDetails";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
-import LoginForm from "./components/login/LoginForm";
 import CartContext from "./components/store/cart-context";
 import ProtectedRoute from "./components/ProductedRoute/ProtectedRoute";
+
+const About = lazy(() => import("./components/About/About"));
+const Home = lazy(() => import("./components/Home/Home"));
+const ContactUs = lazy(() => import("./components/ContactUs/ContactUs"));
+const ProductDetail = lazy(() => import("./components/Products/ProductDetails"));
+const Products = lazy(() => import("./components/Products/Products"));
+const LoginForm = lazy(() => import("./components/login/LoginForm"));
 
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -30,32 +31,34 @@ function App() {
     <BrowserRouter>
       <CartProvider>
         <Header onCartClick={handleCartOpen} />
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route
-            path="/products"
-            element={
-              <ProtectedRoute>
-                <Products />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/products/:productId"
-            element={
-              <ProtectedRoute>
-                <ProductDetail />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          {!authCtx.isLoggedIn && (
-            <Route path="/login" element={<LoginForm />} />
-          )}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<div className="text-center my-5">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route
+              path="/products"
+              element={
+                <ProtectedRoute>
+                  <Products />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/products/:productId"
+              element={
+                <ProtectedRoute>
+                  <ProductDetail />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/about" element={<About />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            {!authCtx.isLoggedIn && (
+              <Route path="/login" element={<LoginForm />} />
+            )}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
         {isCartOpen && <Cart onShow={isCartOpen} onClose={handleClose} />}
         <Footer />
       </CartProvider>
